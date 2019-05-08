360ecf73cb169a6e7003bb54c0772320145633ba
urvish.rana <urvish.rana@volansystech.com>
2019-05-08 17:46:39 +0530

    added signup fragment
    Signed-off-by: urvish.rana <urvish.rana@volansystech.com>

--- app/src/main/java/com/example/awesomenotes/SignUpFragment.kt
+++ app/src/main/java/com/example/awesomenotes/SignUpFragment.kt
@@ -2,11 +2,11 @@ package com.example.awesomenotes
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_sign_up.*
 
 
 /**
@@ -15,13 +15,21 @@ import android.view.ViewGroup
  */
 class SignUpFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
         return inflater.inflate(R.layout.fragment_sign_up, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        googlebutton.setOnClickListener {
+            //todo add firebase code for authenticating user using google account
+        }
+        fbbutton.setOnClickListener {
+            //todo add code for firebase authentication for authenticating user with facebook
+        }
+        signupbutton.setOnClickListener {
+            //todo add code for firebase authentication and possibly add for naviagation chnage.
+        }
+    }
 }
